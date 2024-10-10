b1c8e1603c1a691d469cb6b1e2c92ca7be123fc1
mokhiya <mohiyaxonuzokova@gmail.com>
2024-10-11 03:10:12 +0500

    changes

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -1,15 +1,10 @@
-"""
-WSGI config for conf project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/NajotFeed')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
